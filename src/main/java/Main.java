59a5ed8810c6014801b05d08b87c783a37acb40a
LeonardoTiussMoura <167460740+LeonardoTiussMoura@users.noreply.github.com>
2024-09-02 12:23:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Esse programa serve para calcular a velocidade média em Km/h");
+    System.out.println("");
+    System.out.println("Coloque a distância aqui (em Km):");
+  double distancia = 400;
+    System.out.print(distancia);
+    System.out.println("Km");
+    System.out.println("");
+    System.out.println("Coloque o período de tempo aqui (em horas):");
+      double tempo = 7;
+    System.out.print(tempo);
+    System.out.println("h");
+    System.out.println("");
+    System.out.println("O resultado da distância ÷ tempo é:");
+    double dividir = distancia/tempo;
+    System.out.print(dividir);
+    System.out.println("Km/h");
+    System.out.println("");
+    System.out.println("*O processo de cálculo é pegar a distância, dividir pelo tempo que se obtem a velocidade média.");
+    System.out.println("V=∆s/∆t");
+    System.out.println("");
+    System.out.println("∆= Variação do valor");
+    System.out.println("S= Espaço ou Distância");
+    System.out.println("T= Tempo");
+  }
+}
